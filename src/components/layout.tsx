daa8b0daa1d2da35a2faf74f6727ac02f4eb5dcd
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-07-19 07:35:14 +0900

    Added background images

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,27 @@
 import React, { ReactElement } from 'react'
 
-export default function Layout({
-  title,
-  children,
-}: React.PropsWithChildren<{ title?: string }>): ReactElement {
+export default function Layout({ children }: React.PropsWithChildren<unknown>): ReactElement {
   return (
-    <div>
-      <main>
-        <h1>{title}</h1>
-        {children}
-      </main>
+    <div className="relative h-screen">
+      <main>{children}</main>
 
       <footer></footer>
+
+      <img
+        className="absolute top-0 left-32 h-32 md:right-0"
+        src="/img/red.svg"
+        alt="red background gradient"
+      />
+      <img
+        className="absolute bottom-0 right-0"
+        src="/img/orange.svg"
+        alt="orange background gradient"
+      />
+      <img
+        className="absolute bottom-0 left-0"
+        src="/img/blue.svg"
+        alt="blue background gradient"
+      />
     </div>
   )
 }
